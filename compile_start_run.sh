62cdc8edadc1add7074e6561fbcc1f920d4de3f1
Joey Bloom <jpbloom2@illinois.edu>
2016-10-30 17:38:20 -0500

    Added a script to compile and start the server

---
+++ compile_start_run.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+fuser -k 1099/tcp
+
+javac -cp . simpledb/*/*.java simpledb/*/*/*.java
+javac -cp . studentClient/simpledb/*.java
+
+java simpledb.server.Startup studentdb &
+
+jar cf simpledb.jar  simpledb/*/*.class simpledb/*/*/*.class
+cp simpledb.jar studentClient/simpledb/
+cd studentClient/simpledb 
+java -cp simpledb.jar:. CreateStudentDB
