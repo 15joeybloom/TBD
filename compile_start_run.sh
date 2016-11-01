097ed99b3cfddcab5d1912041a791c8ff4b2d586
Joey Bloom <jpbloom2@illinois.edu>
2016-11-01 15:10:48 -0500

    Split up compilation and db creation

--- compile_start_run.sh
+++ compile_start_run.sh
@@ -6,7 +6,3 @@ javac -cp . studentClient/simpledb/*.java
 
 java simpledb.server.Startup studentdb &
 
-jar cf simpledb.jar  simpledb/*/*.class simpledb/*/*/*.class
-cp simpledb.jar studentClient/simpledb/
-cd studentClient/simpledb 
-java -cp simpledb.jar:. CreateStudentDB
---
+++ create_studentdb.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+jar cf simpledb.jar  simpledb/*/*.class simpledb/*/*/*.class
+cp simpledb.jar studentClient/simpledb/
+cd studentClient/simpledb 
+java -cp simpledb.jar:. CreateStudentDB
+cd ../..
